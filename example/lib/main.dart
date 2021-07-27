160aae57f3ee1b90c5282ff849bdbeb1aee3bd04
Shang <shang1219178163@gmail.com>
2021-07-27 11:14:20 +0800

    代码格式化

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:enhance_stepper/enhance_stepper.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
@@ -33,83 +36,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int groupValue = 0;
-  
+
   StepperType _type = StepperType.vertical;
 
   List<Tuple2> tuples = [
-    Tuple2(Icons.directions_bike, StepState.indexed, ),
-    Tuple2(Icons.directions_bus, StepState.editing, ),
-    Tuple2(Icons.directions_railway, StepState.complete, ),
+    Tuple2(
+      Icons.directions_bike,
+      StepState.indexed,
+    ),
+    Tuple2(
+      Icons.directions_bus,
+      StepState.editing,
+    ),
+    Tuple2(
+      Icons.directions_railway,
+      StepState.complete,
+    ),
     // Tuple2(Icons.directions_boat, StepState.disabled, ),
     // Tuple2(Icons.directions_car, StepState.error, ),
   ];
 
   int _index = 0;
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
-          TextButton(onPressed: (){
-            ddlog("change");
-            setState(() {
-              _type = _type == StepperType.vertical ? StepperType.horizontal : StepperType.vertical;
-            });
-          }, child: Icon(Icons.change_circle_outlined, color: Colors.white,)),
+          TextButton(
+              onPressed: () {
+                // ddlog("change");
+                setState(() {
+                  _type = _type == StepperType.vertical
+                      ? StepperType.horizontal
+                      : StepperType.vertical;
+                });
+              },
+              child: Icon(
+                Icons.change_circle_outlined,
+                color: Colors.white,
+              )),
         ],
         bottom: buildPreferredSize(context),
       ),
-      body: groupValue == 0 ? buildStepper(context) : buildStepperCustom(context),
+      body:
+          groupValue == 0 ? buildStepper(context) : buildStepperCustom(context),
     );
   }
 
   PreferredSizeWidget buildPreferredSize(BuildContext context) {
-    return
-      PreferredSize(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(width: 24),
-                Expanded(
-                  child: CupertinoSegmentedControl(
-                    children: const <int, Widget>{
-                      0: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text('Stepper', style: TextStyle(fontSize: 15))),
-                      1: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text('enhance_stepper', style: TextStyle(fontSize: 15))),
-                    },
-                    groupValue: groupValue,
-                    onValueChanged: (value) {
-                      // TODO: - fix it
-                      ddlog(value.toString());
-                      setState(() {
-                        groupValue = int.parse("$value");
-                      });
-                    },
-                    borderColor: Colors.white,
-                    selectedColor: Colors.white,
-                    unselectedColor: Colors.blue,
-                  ),
+    return PreferredSize(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8, bottom: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(width: 24),
+              Expanded(
+                child: CupertinoSegmentedControl(
+                  children: const <int, Widget>{
+                    0: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Stepper', style: TextStyle(fontSize: 15))),
+                    1: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('enhance_stepper',
+                            style: TextStyle(fontSize: 15))),
+                  },
+                  groupValue: groupValue,
+                  onValueChanged: (value) {
+                    // TODO: - fix it
+                    // ddlog(value.toString());
+                    setState(() {
+                      groupValue = int.parse("$value");
+                    });
+                  },
+                  borderColor: Colors.white,
+                  selectedColor: Colors.white,
+                  unselectedColor: Colors.blue,
                 ),
-                SizedBox(width: 24)
-              ],
-            ),
+              ),
+              SizedBox(width: 24)
+            ],
           ),
-          preferredSize: Size(double.infinity, 48));
+        ),
+        preferredSize: Size(double.infinity, 48));
   }
 
   void go(int index) {
-    if (index == -1 && _index <= 0 ) {
+    if (index == -1 && _index <= 0) {
       ddlog("it's first Step!");
       return;
     }
@@ -129,13 +146,17 @@ class _MyHomePageState extends State<MyHomePage> {
         type: _type,
         currentStep: _index,
         physics: ClampingScrollPhysics(),
-        steps: tuples.map((e) => Step(
-          state: StepState.values[tuples.indexOf(e)],
-          isActive: _index == tuples.indexOf(e),
-          title: Text("step ${tuples.indexOf(e)}"),
-          subtitle: Text("${e.item2.toString().split(".").last}",),
-          content: Text("Content for Step ${tuples.indexOf(e)}"),
-        )).toList(),
+        steps: tuples
+            .map((e) => Step(
+                  state: StepState.values[tuples.indexOf(e)],
+                  isActive: _index == tuples.indexOf(e),
+                  title: Text("step ${tuples.indexOf(e)}"),
+                  subtitle: Text(
+                    "${e.item2.toString().split(".").last}",
+                  ),
+                  content: Text("Content for Step ${tuples.indexOf(e)}"),
+                ))
+            .toList(),
         onStepCancel: () {
           go(-1);
         },
@@ -148,21 +169,27 @@ class _MyHomePageState extends State<MyHomePage> {
             _index = index;
           });
         },
-        controlsBuilder: (BuildContext context, { VoidCallback? onStepContinue, VoidCallback? onStepCancel }){
+        controlsBuilder: (BuildContext context,
+            {VoidCallback? onStepContinue, VoidCallback? onStepCancel}) {
           return Row(
             children: [
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: onStepContinue,
                 child: Text("Next"),
               ),
-              SizedBox(width: 8,),
-              TextButton(onPressed: onStepCancel, child: Text("Back"), ),
-
+              SizedBox(
+                width: 8,
+              ),
+              TextButton(
+                onPressed: onStepCancel,
+                child: Text("Back"),
+              ),
             ],
           );
-        }
-    );
+        });
   }
 
   Widget buildStepperCustom(BuildContext context) {
@@ -173,14 +200,22 @@ class _MyHomePageState extends State<MyHomePage> {
         horizontalLinePosition: HorizontalLinePosition.top,
         currentStep: _index,
         physics: ClampingScrollPhysics(),
-        steps: tuples.map((e) => EnhanceStep(
-          circleChild: Icon(e.item1, color: Colors.blue, size: 30,),
-          state: StepState.values[tuples.indexOf(e)],
-          isActive: _index == tuples.indexOf(e),
-          title: Text("step ${tuples.indexOf(e)}"),
-          subtitle: Text("${e.item2.toString().split(".").last}",),
-          content: Text("Content for Step ${tuples.indexOf(e)}"),
-        )).toList(),
+        steps: tuples
+            .map((e) => EnhanceStep(
+                  circleChild: Icon(
+                    e.item1,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                  // state: StepState.values[tuples.indexOf(e)],
+                  isActive: _index == tuples.indexOf(e),
+                  title: Text("step ${tuples.indexOf(e)}"),
+                  subtitle: Text(
+                    "${e.item2.toString().split(".").last}",
+                  ),
+                  content: Text("Content for Step ${tuples.indexOf(e)}"),
+                ))
+            .toList(),
         onStepCancel: () {
           go(-1);
         },
@@ -193,23 +228,26 @@ class _MyHomePageState extends State<MyHomePage> {
             _index = index;
           });
         },
-        controlsBuilder: (BuildContext context, { VoidCallback? onStepContinue, VoidCallback? onStepCancel }){
+        controlsBuilder: (BuildContext context,
+            {VoidCallback? onStepContinue, VoidCallback? onStepCancel}) {
           return Row(
             children: [
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: onStepContinue,
                 child: Text("Next"),
               ),
-              SizedBox(width: 8,),
+              SizedBox(
+                width: 8,
+              ),
               TextButton(
                 onPressed: onStepCancel,
                 child: Text("Back"),
               ),
             ],
           );
-        }
-    );
+        });
   }
 }
-
